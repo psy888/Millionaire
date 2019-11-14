4f6da03cb174f6e4c986aebf0bb809e8d5376246
psy888 <tbilisi.zp.snab@gmail.com>
2019-11-14 21:33:34 +0200

    add base logic

--- src/com/psy888/Game.java
+++ src/com/psy888/Game.java
@@ -1,4 +1,48 @@
 package com.psy888;
 
 public class Game {
+    Question[] questions; //15 вопросов
+    int curQuestion;
+    String userName;
+    int [] prize;
+    UI ui; //User interface
+
+
+    {
+        this.ui = new UI();
+        this.prize = new int[]{100,200,300,500,1000,2000,4000,8000,16000,32000,64000,125000,250000,500000,1000000};
+    }
+    //Пустой
+    public Game() {
+    }
+
+
+    public Game(String name) {
+        this.userName = name;
+    }
+
+
+
+    public void start(){
+        System.out.println("============================================================================");
+        System.out.println("======================= Как стать миллионером? =============================");
+        System.out.println("============================================================================");
+    }
+
+    private void shuffleQuestions(){
+
+    }
+
+    public void ask(int i){
+        ui.printQuestion(questions[i]);
+    }
+
+    public boolean isRightAnswer(Question q, int index){
+        if(q.rightAnswer == index){
+            return true;
+        }
+        return false;
+    }
+
+
 }
---
+++ src/com/psy888/UI.java
@@ -0,0 +1,42 @@
+package com.psy888;
+
+public class UI {
+
+
+    public void printQuestion(Question q){
+        System.out.println("Внимание вопрос :");
+        System.out.println(q.getQuestion());
+
+        for (int i = 0; i < q.getAnswers().length; i++) {
+            String answer = getLetter(i) + "). " + q.getAnswers()[i];
+            if (i % 2 == 0) {
+                System.out.print(answer + "\t\t");
+            } else {
+                System.out.println(answer + "\n");
+            }
+        }
+    }
+
+
+    public String getLetter (int index){
+        String letter;
+        switch (index){
+            case 0:
+                letter = "A";
+                break;
+            case 1:
+                letter = "B";
+                break;
+            case 2:
+                letter = "C";
+                break;
+            case 3:
+                letter = "D";
+                break;
+            default:
+                letter="Z"; // просто так
+
+        }
+        return letter;
+    }
+}
